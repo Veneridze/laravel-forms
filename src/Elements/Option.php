45b90da66f6bee6474de082b09cc0849e02f9087
Veneridze <viacheslav.kopaev@yandex.ru>
2024-10-06 12:16:53 +0300

    Update Option.php

--- src/Elements/Option.php
+++ src/Elements/Option.php
@@ -3,14 +3,14 @@ namespace Veneridze\LaravelForms\Elements;
 
 final class Option {
     public function __construct(
-        public string $label,
-        public string $value,
+        public ?string $label,
+        public ?string $value,
         public array $availableif = []
     ) {}
 
     public function toArray(): array {
         return [
-            'label' => $this->label,
+            'label' => $this->label ?? $this->value,
             'value' => $this->value,
             'availableif' => $this->availableif
         ];
