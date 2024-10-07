32e7f9b4f8043e1eaf2f035d68def58afaa6a737
Veneridze <viacheslav.kopaev@yandex.ru>
2024-10-07 23:28:52 +0300

    Update Option.php

--- src/Elements/Option.php
+++ src/Elements/Option.php
@@ -4,14 +4,14 @@ namespace Veneridze\LaravelForms\Elements;
 final class Option {
     public function __construct(
         public ?string $label,
-        public ?string $value,
+        public mixed $value,
         public array $availableif = []
     ) {}
 
     public function toArray(): array {
         return [
             'label' => $this->label ?? $this->value,
-            'value' => $this->value,
+            'value' => is_numeric($this->value) ? (int)$this->value : $this->value,
             'availableif' => $this->availableif
         ];
     }
