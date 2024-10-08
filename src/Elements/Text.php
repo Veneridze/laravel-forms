1b3370d36d37e03b025bbaa4f09e17cd93a8cf46
Veneridze <viacheslav.kopaev@yandex.ru>
2024-10-09 01:19:49 +0300

    Update Text.php

--- src/Elements/Text.php
+++ src/Elements/Text.php
@@ -10,6 +10,7 @@ class Text extends Input implements Element {
         public bool $disabled = false,
         public string $type = 'text',
         public array $visibleif = [],
+        public string $prefix = null,
         public null|string $mask = null,
         public ?int $maxlength = null,
         public ?string $placeholder = null,
