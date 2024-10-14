efff263e0ef9a5dabc0c3670b4cc6bb7fdecf90b
Veneridze <viacheslav.kopaev@yandex.ru>
2024-10-14 16:31:28 +0300

    Create File.php

---
+++ src/Elements/File.php
@@ -0,0 +1,24 @@
+<?php
+namespace Veneridze\LaravelForms\Elements;
+use Veneridze\LaravelForms\Interfaces\Element;
+use Veneridze\LaravelForms\Prototype\Input;
+
+class File extends Input implements Element {
+    public function __construct(
+        public string $label,
+        public string $key,
+        public ?string $accept = null,
+        public bool $disabled = false,
+        public array $visibleif = [],
+    ) {}
+    public function toArray(): array {
+        return [
+            'type' => 'file',
+            'label' => $this->label,
+            'accept' => $this->accept,
+            'disabled' => $this->disabled,
+            'key' => $this->key,
+            'visibleif' => $this->visibleif
+        ];
+    }
+}
