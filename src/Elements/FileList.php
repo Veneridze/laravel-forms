5cbf3981c59b460e809147033358c8152fe2c249
Viacheslav Kopaev <viacheslav.kopaev@yandex.ru>
2025-03-09 03:00:38 +0300

    Create FileList.php

---
+++ src/Elements/FileList.php
@@ -0,0 +1,32 @@
+<?php
+namespace Veneridze\LaravelForms\Elements;
+use Veneridze\LaravelForms\Interfaces\Element;
+use Veneridze\LaravelForms\Prototype\Input;
+
+class FileList extends Input implements Element
+{
+    public string $type = 'filelist';
+    public function __construct(
+        public string $label,
+        public string $key,
+        public string $source,
+        public int $limit = 1,
+        public ?string $accept = null,
+        public array $displayifset = [],
+        public array $visibleif = [],
+    ) {
+    }
+    public function toArray(): array
+    {
+        return [
+            'type' => 'filelist',
+            'label' => $this->label,
+            'accept' => $this->accept,
+            'source' => $this->source,
+            'limit' => $this->limit,
+            'key' => $this->key,
+            'visibleif' => $this->visibleif,
+            'displayifset' => $this->displayifset
+        ];
+    }
+}
